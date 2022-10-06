8e3dcac756f4501795f7349fc5731f767a79da3e
arshiamidos <arshiamidos@gmail.com>
2022-10-06 15:57:57 +0200

    fix chr

--- chr.js
+++ chr.js
@@ -2,11 +2,12 @@ var fs = require('fs');
 var path = require('path');
 var rom = fs.readFileSync(path.join(__dirname, "./rom/mario.nes"));
 let CHR = []
-// console.log(rom)
+
 function createCHR(rom, CHR) {
     const Z = 16 + (16384 * rom[4])
     let i = 0
-    while (i < 8192 * rom[5] - 8) {
+    let k = 0
+    while (i < 8192 * rom[5]) {
         for (let j = 0; j < 8; j++) {
             let extracted1 = [...rom[Z + i + j + 0].toString(2).padStart(8, '0')]
             let extracted2 = [...rom[Z + i + j + 8].toString(2).padStart(8, '0')]
@@ -14,94 +15,35 @@ function createCHR(rom, CHR) {
             let extracted3 = extracted1.map((char, index) => char + extracted2[index])
             extracted3.filter((char, index) => {
                 if (char == '00') extracted3[index] = '⚫️'
-                if (char == '10') extracted3[index] = '🔴'
+                if (char == '10') extracted3[index] = '🤢'
                 if (char == '01') extracted3[index] = '🔵'
                 if (char == '11') extracted3[index] = '⚪️'
             })
             extracted3 = extracted3.join('')
-            CHR[i + j] = extracted3
+            CHR[k + j] = extracted3
         }
 
-        i = i + 8
+        i = i + 16
+        k = k + 8
     }
 }
 createCHR(rom, CHR)
-function printCHR(rom, CHR) {
-    let i = 0
-    while (i < 8192 * rom[5] - 8) {
-        for (let j = 0; j < 8; j++) {
-            console.log(CHR[i + j]);
-        }
-        console.log(i - 8, i)
-        i += 8
-    }
 
-}
-
-function printCHR2(rom, CHR) {
-    for (let k = 0; k < CHR.length; k = k + 16 * 8) {
+function printCHR2(CHR) {
+    for (let k = 0; k < CHR.length; k = k + 32 * 8) {
         for (let j = 0; j < 8; j++) {
-            process.stdout.write(CHR[k + j + 00 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 01 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 02 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 03 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 04 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 05 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 06 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 07 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 08 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 09 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 10 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 11 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 12 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 13 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 14 * 8] + "  ");
-            process.stdout.write(CHR[k + j + 15 * 8] + "  ");
+            for (let i = 0; i < 32; i++) {
+                if (i % 2 == 0) {
+                    process.stdout.write(CHR[k + j + i * 8] + "  ");
+                } else {
+                    process.stdout.write(CHR[k + j + i * 8] + "  ");
+                }
+            }
             console.log()
         }
         console.log()
     }
 }
 
-
-function check() {
-    const rom = fs.readFileSync(path.join(__dirname, "./rom/dk.nes"));
-    const nesHeader = (rom[0] << 24 + rom[1] << 16 + rom[2] << 8 + rom[3]) & 0xffffffff;
-    if (!nesHeader == 0x1a53454e) {
-        throw Error("unmatched nes header")
-    }
-    this.prgCount = rom[4] // PRG-ROM page count
-    this.chrCount = rom[5] // CHR-ROM page count
-    this.control1 = rom[6] // mapper mirroring battery trainer
-    this.control2 = rom[7] // mapper mirroring battery trainer
-
-    const mapper1 = this.control1 >> 4
-    const mapper2 = this.control2 >> 4
-    this.mapper = mapper1 | mapper2 << 4
-
-    const mirror1 = this.control1 & 1
-    const mirror2 = (this.control1 >> 3) & 1
-    this.mirror = mirror1 | mirror2 << 1
-
-    const Z = 16 + (16384 * this.prgCount)
-
-    this.chr = [];
-    this.prg = [];
-
-    for (let i = 16; i < Z; i++) {
-        this.prg.push(rom[i])
-    }
-
-    for (let i = Z; i < Z + 8192 * this.chrCount; i++) {
-        this.chr.push(rom[i])
-    }
-
-    if (this.chrCount == 0) {
-        this.chr = new Array(8129).fill(0x00)
-    }
-    console.log(this.mapper)
-    return rom
-}
-// check()
-// printCHR(rom, CHR)
-printCHR2(rom, CHR)
+console.log(CHR.length)
+printCHR2(CHR)
